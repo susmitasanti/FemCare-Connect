f94290f60bd7153b7e9f8c0fbf2f86aaf125542c
susmitasanti <2021.susmita.santi@ves.ac.in>
2023-09-18 00:47:53 +0530

    adding_Pharmacy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import Pharmacy from './components/Pharmacy';
 import Tracker from './components/Tracker';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <>
-    <Tracker/>
+    <BrowserRouter>
+<Routes>
+  <Route path='/' element={<Tracker/>}></Route>
+  <Route path='/pharmacy' element={<Pharmacy/>}></Route>
+</Routes>
+    
+    
+    </BrowserRouter>
     </>
   );
 }
