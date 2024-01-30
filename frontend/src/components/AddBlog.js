51b44dcf721c2a086b2f60e7f8dd37b01f3068de
rish88c <rishg98@gmail.com>
2024-01-30 09:23:29 +0530

    Added script in index.html

--- frontend/src/components/AddBlog.js
+++ frontend/src/components/AddBlog.js
@@ -1,15 +1,7 @@
 import React from 'react'
 import Notes from './Notes'
-import { useEffect } from 'react';
-import ReactGA from 'react-ga';
 
 function AddBlog() {
-
-  useEffect(() => {
-    ReactGA.initialize('G-PMZ0QJLDPD');
-    // To record a page view
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
   return (
     <div>
       <Notes />
