8c2ded6d601cbe5c96bc5a79498611662c19c32b
susmitasanti <2021.susmita.santi@ves.ac.in>
2024-05-30 15:53:22 +0530

    Updated maps.

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,26 +1,34 @@
-import React, { useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
-import { Icon } from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import '../css/Login.css'; // Import your custom CSS for styling
+import React, { useState, useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
+import { Icon } from "leaflet";
+import "leaflet/dist/leaflet.css";
+import locationIconUrl from "../css/location.png";
+import hospitalIconUrl from "../css/hospiloc.png";
+import Classes from "../css/map.css";
 
-function LocationMarker() {
+const API_URL = "http://localhost:5000/api/HospMap/search";
+
+function LocationMarker({ lat, lon }) {
   const [position, setPosition] = useState(null);
   const map = useMapEvents({
-    click() {
-      map.locate();
-    },
     locationfound(e) {
       setPosition(e.latlng);
       map.flyTo(e.latlng, map.getZoom());
     },
   });
 
+  useEffect(() => {
+    if (lat && lon) {
+      setPosition([lat, lon]);
+      map.flyTo([lat, lon], map.getZoom());
+    }
+  }, [lat, lon, map]);
+
   const customIcon = new Icon({
-    iconUrl: 'images/placeholder.png', // Relative path to your custom icon image
-    iconSize: [30, 30], // Set the width and height of your icon
-    iconAnchor: [12, 41], // Set the anchor point
-    popupAnchor: [1, -34], // Set the popup anchor
+    iconUrl: locationIconUrl,
+    iconSize: [30, 30],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
   });
 
   return position === null ? null : (
@@ -30,74 +38,122 @@ function LocationMarker() {
   );
 }
 
-function Map() {
-  const center = [51.505, -0.09];
- 
-  const customIcon = new Icon({
-    iconUrl: 'images/location1.png', // Relative path to your custom icon image
-    iconSize: [30, 30], // Set the width and height of your icon
-    iconAnchor: [12, 41], // Set the anchor point
-    popupAnchor: [1, -34], // Set the popup anchor
+function HospMap() {
+  const [locations, setLocations] = useState([]);
+  const [locationInput, setLocationInput] = useState("");
+  const [lat, setLat] = useState(null);
+  const [lon, setLon] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLat(position.coords.latitude);
+          setLon(position.coords.longitude);
+        },
+        (error) => {
+          console.error("Error fetching current location:", error);
+        }
+      );
+    }
+  }, []);
+
+  const search = async () => {
+    try {
+      if (locationInput === "") return;
+
+      setLoading(true);
+
+      // Fetch coordinates from a location input
+      const geocodeResponse = await fetch(`https://nominatim.openstreetmap.org/search?q=${locationInput}&format=json`);
+      const geocodeData = await geocodeResponse.json();
+      if (geocodeData.length === 0) throw new Error("No location found");
+
+      const { lat, lon } = geocodeData[0];
+      setLat(lat);
+      setLon(lon);
+
+      // Fetch nearby hospitals
+      const response = await fetch(`${API_URL}?lat=${lat}&lng=${lon}&category=hospitals`);
+      if (!response.ok) throw new Error("Network response was not ok");
+
+      const locationData = await response.json();
+      setLocations(locationData);
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const hospitalIcon = new Icon({
+    iconUrl: hospitalIconUrl,
+    iconSize: [30, 30],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
   });
 
-  // Array of gynecologist locations with latitude, longitude, and name
-  const gynecologistLocations = [
-    {
-      lat: 19.056573, lng: 72.889541, name: 'Gynecologist A'
-    },
-    { lat:19.042864, lng: 72.914827, name: 'Gynecologist B' },
-    { lat:19.053555, lng: 72.887009, name: 'Gynecologist C' },
-    { lat:19.056553, lng: 72.890400, name: 'Gynecologist D' },
-    { lat:19.047496, lng: 72.874586, name: 'Gynecologist E' },
-    // { lat:19.046283, lng: 72.842670, name: 'Gynecologist F' },
-
-  
-    // Add more locations as needed
-  ];
-
   return (
-    <div className='container'>
-      <MapContainer
-        center={center}
-        zoom={13}
-        scrollWheelZoom={false}
-        style={{ height: '500px', width: '100%' }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {gynecologistLocations.map((location, index) => (
-          <Marker
-            key={index}
-            position={[location.lat, location.lng]} icon={customIcon}
-          >
-            <Popup>{location.name}</Popup>
-          </Marker>
-        ))}
-        <LocationMarker />
-      </MapContainer>
-    </div>
+    <section className={Classes.location}>
+      <div className={Classes.container}>
+        <h2 style={{ width: "100%", textAlign: "center", marginBottom: "20px" }}>
+          Find Nearby Hospitals
+        </h2>
+
+        <div className="searchBox">
+          <div className="inputContainer">
+            <input
+              type="text"
+              placeholder="Enter location"
+              value={locationInput}
+              onChange={(e) => setLocationInput(e.target.value)}
+            />
+            <button onClick={search} style={{ color: "white", backgroundColor: " #144c52" }}>
+              Search
+            </button>
+          </div>
+        </div>
+
+        {loading && (
+          <div className={Classes.loadingMessage}>
+            It might take a few seconds to find nearby hospitals...
+          </div>
+        )}
+
+        <MapContainer
+          center={[lat || 0, lon || 0]}
+          zoom={13}
+          scrollWheelZoom={false}
+          style={{ height: "500px", width: "100%" }}
+          className={Classes.leafletcontainer}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {locations.map((location, index) => (
+            <Marker
+              key={index}
+              position={[location.lat, location.lng]}
+              icon={hospitalIcon}
+            >
+              <Popup>
+                <div>
+                  <h3>{location.name}</h3>
+                  <p>Address: {location.address}</p>
+                  <p>Rating: {location.rating}</p>
+                  <p>Phone: {location.phone}</p>
+                  <p>Website: {location.website}</p>
+                </div>
+              </Popup>
+            </Marker>
+          ))}
+          <LocationMarker lat={lat} lon={lon} />
+        </MapContainer>
+      </div>
+    </section>
   );
 }
 
-export default Map;
-
-//Near Pixolo
-// {
-//   lat: 19.090880, lng: 72.913788, name: 'Gynecologist A'
-// },
-// { lat:19.093265, lng: 72.912952, name: 'Gynecologist B' },
-// { lat:19.090449, lng: 72.907330, name: 'Gynecologist C' },
-// { lat:19.093261, lng: 72.913045, name: 'Gynecologist D' },
-// { lat:19.095297, lng: 72.913895, name: 'Gynecologist E' },
-
-//Near Home
-// {
-//   lat: 19.040306, lng: 72.851998, name: 'Gynecologist A'
-// },
-// { lat:19.039987, lng: 72.854652, name: 'Gynecologist B' },
-// { lat:19.040241, lng: 72.851997, name: 'Gynecologist C' },
-// { lat:19.040388, lng: 72.854811, name: 'Gynecologist D' },
-// { lat:19.043134, lng: 72.856226, name: 'Gynecologist E' },
-// { lat:19.046283, lng: 72.842670, name: 'Gynecologist F' },
+export default HospMap;
